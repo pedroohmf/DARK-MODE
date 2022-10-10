ae01e22137fe591c3869e2f7fa89e51350c8e315
Pedro Henrique <pedrohfm2012@yahoo.com.br>
2022-10-10 13:53:26 -0300

    Finalizando Dark Mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log("Teste");
+const changeThemeBtn = document.querySelector("#change-theme");
+
+// Toggle dark mode
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
+
+// Load Light or Dark Mode
+function loadTheme() {
+    const darkMode = localStorage.getItem("dark");
+    if (darkMode) {
+        toggleDarkMode();
+    }
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function() {
+    toggleDarkMode();
+
+    // Save or remove Dark mode
+    localStorage.removeItem("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("dark", 1);
+    }
+});
